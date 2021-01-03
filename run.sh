09a55806231682a54a6346fd1c21b2139b2adf33
MauritsvanderZee <maurits@van-der-zee.de>
2021-01-04 00:23:03 +0100

    updated automatic compile script

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 while true
 do 
-	inotifywait -e modify report.tex chapters/*.tex
+	inotifywait -e modify report.tex chapters/*.tex appendices/*.tex bibliography/*.bib frontmatter/*.tex preamble/*.tex
 	echo "Starting to compile..."
 	make > /dev/null 2> /dev/null
 	echo "Compilation done"
